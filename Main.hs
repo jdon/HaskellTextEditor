2982726589739b11e4a8749ca4af7031bc163284
Jonathan Donaldson <jdonaldson278@gmail.com>
2017-04-30 16:20:31 +0100

    cut,paste,copy

--- Main.hs
+++ Main.hs
@@ -5,7 +5,9 @@ type B = [Char]
 type C = [Char]
 
 data TextEdit = TextEdit L S A B deriving (Show)
-
+t = (TextEdit "sdad" "dsad" "oppj" "dsadasd")
+e = (TextEdit "sdad" "" "oppj" "dsadasd")
+f = (TextEdit "sdad" "" "oppj" "")
 getLeftCharacters:: TextEdit -> L
 getLeftCharacters (TextEdit l _ _ _) = l
 
@@ -31,9 +33,26 @@ setCopyBufferCharacters:: TextEdit -> B -> TextEdit
 setCopyBufferCharacters (TextEdit l s a _) j = (TextEdit l s a j)
 
 insertCharacter:: TextEdit -> C -> TextEdit
-insertCharacter (TextEdit l _ a b) c = (TextEdit (l ++ [head c]) [] a b)
+insertCharacter (TextEdit l s a b) c
+    |length (l ++ a ++ c) <= 1024 = (TextEdit (l ++ [head c]) [] a b)
+    |otherwise = (TextEdit l s a b)
 
 backspace :: TextEdit -> TextEdit
 backspace (TextEdit l s a b)
-   | s == [] = (TextEdit(reverse (tail (reverse l))) [] a b)
-   | otherwise (TextEdit l [] a b)
+    | s == [] = (TextEdit(reverse (tail (reverse l))) [] a b)
+    | otherwise = (TextEdit l [] a b)
+
+delete :: TextEdit -> TextEdit
+delete (TextEdit l s a b)
+    | s == [] = (TextEdit l s (tail a) b)
+    | otherwise = (TextEdit l [] a b)
+copy :: TextEdit -> TextEdit
+copy (TextEdit l s a _) = (TextEdit l s a s)
+
+paste :: TextEdit -> TextEdit
+paste (TextEdit l s a b)
+    |length (l ++ a ++ b) <= 1024 = (TextEdit (l ++ b) [] a b)
+    |otherwise = (TextEdit l s a b)
+
+cut :: TextEdit -> TextEdit
+cut (TextEdit l s a b) = (TextEdit l [] a s)
